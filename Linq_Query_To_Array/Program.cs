01e9f07abdf2a3b177f8f4a4610875ed1a6dcd43
Melodie Joseph <josmelodie@gmail.com>
2019-09-10 23:04:11 +1200

    Simple Linq Query to Array

---
+++ Linq_Query_To_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_Query_To_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Data Source
+            string[] subjects = { "Art", "Programming", "Music", "Chemistry" };
+            //Linq Query
+            var query = from subject in subjects
+                        where subject.Contains('m')
+                        select subject;
+
+            //Query execution
+            foreach (var subject in query)
+            {
+                Console.WriteLine(subject + " ");
+            }
+        }
+    }
+}
