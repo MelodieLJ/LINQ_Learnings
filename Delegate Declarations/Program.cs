d37b8923b335f4c74e877b186eb039a32bc190be
Melodie Joseph <josmelodie@gmail.com>
2019-10-03 21:52:08 +1300

    Delegate Declarations: Four ways of of declaring delegate variables

---
+++ Delegate Declarations/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Declarations
+{
+    class Program
+    {
+        // A delegate is simply a reference to a method
+        //The Greetings type below is a reference to a method that tales a string and returns a string
+        public delegate string Greetings(string result);
+
+        static void Main(string[] args)
+        {
+            string entry = Console.ReadLine();
+            Console.WriteLine("First Example: " + greetingsFunc1(entry));
+            Console.WriteLine("Second Example: " + greetingsFunc2(entry));
+            Console.WriteLine("Third Example: " + greetingsFunc3(entry));
+            Console.WriteLine("Fourth Example: " + greetingsFunc4(entry));
+        }
+        //The Greetings type can be used to declare variables in several ways:
+
+        //
+        public static string Greet(string result)
+        {
+            return "Hello, " + result + "!";
+        }
+        public static Greetings greetingsFunc1 = Greet;
+
+        public static Greetings greetingsFunc2 = delegate (string result)
+        {
+            return "Hello, " + result + "!";
+        };
+
+        //Lamba expressions are a more efficient way of defining a delegate
+        public static Greetings greetingsFunc3 = (result) =>
+        {
+            return "Hello, " + result + "!";
+        };
+
+        //If the only statement in the lamba expression is the return statement, the shortened form below can be used
+        public static Greetings greetingsFunc4 = (result) => "Hello, " + result + "!";
+
+    }
+}
